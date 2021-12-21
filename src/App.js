6526377c11e451bb7de834c3180e9f55a5692808
Sam Elmer <selmer@tamu.edu>
2021-12-20 19:27:46 -0600

    Test component + fix dependencies
    Created a test calculator component to figure out component classes
    
    Added material UI as a dependency & fixed react scripts dependency warnings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Calculator } from './components/Calculator.js'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Calculator></Calculator>
       </header>
     </div>
   );
---
+++ src/components/Calculator.js
@@ -0,0 +1,17 @@
+import { Container } from "@mui/material";
+import React from "react";
+
+export class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <Container maxwidth="sm">
+        <p>Test Text</p>
+      </Container>
+    )
+  }
+}
